13174466cff243bd340901eb1c8c5b00dcc43adc
MkPoutato <mkpoutato@gmail.com>
2026-05-01 19:35:29 -0400

    Update data_preprocessing.py with new imports
    Added imports for io and requests. Removed git command and added a placeholder for GITHUB_RAW_URL.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 import torch as nn
+import io
+import requests
 import TensorFlow as tf
 from sklearn.model_section import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder, OneHotEncoder
@@ -11,6 +13,5 @@ from github import
 import warnings
 warnings.filterWarnings('ignore')
 
-git add Pancreatic_data(1).xlsx
-
+GITHUB_RAW_URL= "
 print (ds.head)
